1c02fcd514419891e1995c5e3e209d6eb9c56a23
maxastuart <maxastuart@gmail.com>
2019-11-10 23:36:10 +0000

    WIP, additional tests in 3-main

--- test_base_model.py
+++ test_base_model.py
@@ -7,6 +7,8 @@ my_model.my_number = 89
 print(my_model)
 my_model.save()
 print(my_model)
+print("-----")
+print(my_model.to_dict())
 my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
