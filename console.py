d39a8ddcf22c3a9aca22a2b61870d37f564ff0ed
maxastuart <maxastuart@gmail.com>
2019-11-11 00:43:26 +0000

    Fix 6. Console 0.0.1

--- console.py
+++ console.py
@@ -2,16 +2,18 @@
 """console for airbnb project"""
 import cmd
 
-class HBNBCommand(cmd.Cmd):
 
+class HBNBCommand(cmd.Cmd):
     """class for command module"""
+    prompt = "(hbnb) "
 
     def do_EOF(self, line):
-       return True
+        """EOF command to exit the program"""
+        return True
 
     def do_quit(self, line):
-       return True
+        """Quit command to exit the program"""
+        return True
 
-    def do_prompt(self):
-       "Change the interactive prompt"
-       self.prompt = "(hbnb)"
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
