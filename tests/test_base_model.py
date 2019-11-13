e6abe370e75eaa862f777d52b92e752e9027f0b7
maxastuart <maxastuart@gmail.com>
2019-11-13 20:57:48 +0000

    All First Round Tests for BaseModel, Update .gitignore

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -10,8 +10,8 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     """ Tests the BaseModel class's methods and attributes. """
 
-    """ sets kwargs properly """
     def setUp(self):
+        """ sets kwargs properly """
         self.base1 = BaseModel(number=89,
                           created_at="2019-11-13T01:25:18.335269",
                           updated_at="2019-11-13T01:25:18.335279",
@@ -52,13 +52,33 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('number', self.base1.__dict__.keys())
 
     def test_save(self):
-        pass
+        self.assertEqual(self.base1.updated_at,
+                         datetime.datetime(2019, 11, 13, 1, 25, 18, 335279))
+        self.base1.save()
+        self.assertNotEqual(self.base1.updated_at,
+                            datetime.datetime(2019, 11, 13, 1, 25, 18, 335279))
+        self.assertEqual(type(self.base1.updated_at), datetime.datetime)
 
     def test_save_instance_attrs(self):
-        pass
+        self.base1.save()
+        self.assertEqual(self.base1.number, 89)
 
     def test_to_dict(self):
-        pass
+        dict1 = {'number': 89,
+                 '__class__': "BaseModel",
+                 'created_at': "2019-11-13T01:25:18.335269",
+                 'updated_at': "2019-11-13T01:25:18.335279",
+                 'id': "0e5ad480-ebf5-4bc8-9771-2a0e8daff35d"}
+        self.assertEqual(self.base1.to_dict(), dict1)
 
     def test_to_dict_instance_attrs(self):
-        pass
+        self.base1.astring = "pew! pew pew!!!"
+        self.base1.afloat = 9.87654321
+        dict1 = {'number': 89,
+                 'astring': "pew! pew pew!!!",
+                 'afloat': 9.87654321,
+                 '__class__': "BaseModel",
+                 'created_at': "2019-11-13T01:25:18.335269",
+                 'updated_at': "2019-11-13T01:25:18.335279",
+                 'id': "0e5ad480-ebf5-4bc8-9771-2a0e8daff35d"}
+        self.assertEqual(self.base1.to_dict(), dict1)
