a157531fcc12f2501b7c76ef0f625ad3f546e210
maxastuart <maxastuart@gmail.com>
2019-11-13 09:48:33 +0000

    improve test_init_defaults

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -3,6 +3,7 @@
 """
 import unittest
 import datetime
+import uuid
 from models.base_model import BaseModel
 
 
@@ -15,6 +16,8 @@ class TestBaseModel(unittest.TestCase):
                           created_at="2019-11-13T01:25:18.335269",
                           updated_at="2019-11-13T01:25:18.335279",
                           id="0e5ad480-ebf5-4bc8-9771-2a0e8daff35d")
+        self.base2 = BaseModel()
+        self.base3 = BaseModel()
 
     def test_init_insufficient_kwargs(self):
         with self.assertRaises(Exception):
@@ -28,12 +31,15 @@ class TestBaseModel(unittest.TestCase):
                          datetime.datetime(2019, 11, 13, 1, 25, 18, 335279))
 
     def test_init_defaults(self):
-        base2 = BaseModel()
-        base3 = BaseModel()
-        self.assertIs(type(base2.created_at), datetime.datetime)
-        self.assertIs(type(base3.created_at), datetime.datetime)
-        self.assertNotEqual(base2, base3)
-        
+        self.assertIs(type(self.base2.created_at), datetime.datetime)
+        self.assertIs(type(self.base3.created_at), datetime.datetime)
+        self.assertIs(type(self.base2.updated_at), datetime.datetime)
+        self.assertIs(type(self.base3.updated_at), datetime.datetime)
+        self.assertIs(type(self.base2.id), str)
+        self.assertIs(type(self.base3.id), str)
+        self.assertNotEqual(self.base2.created_at, self.base3.created_at)
+        self.assertNotEqual(self.base2.updated_at, self.base3.updated_at)
+        self.assertNotEqual(self.base2.id, self.base3.id)
 
     def test_str(self):
         pass
