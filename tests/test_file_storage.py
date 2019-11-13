be1ec2d31eb3af0b4aae7b2b78706b879dc9794e
maxastuart <maxastuart@gmail.com>
2019-11-13 05:38:38 +0000

    create file storage tests template

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -9,7 +9,48 @@ from models.engine.file_storage import FileStorage
 class TestFileStorage(unittest.TestCase):
     """ Tests the FileStorage class's methods and attributes. """
 
-    pass
+    def test_all_empty(self):
+        
+
+    def test_all(self):
+        
+
+    def test_all_instance_attrs(self):
+        
+
+    def test_new_empty(self):
+        
+
+    def test_new(self):
+        
+
+    def test_new_bad_obj(self):
+        """tests for lacking class.__name__ and lacking .id"""
+        
+
+    def test_save_first(self):
+        
+
+    def test_save_additional(self):
+        
+
+    def test_save_single_obj(self):
+        
+
+    def test_save_multiple_objs(self):
+        
+
+    def test_reload_file_nonexist(self):
+        
+
+    def test_reload(self):
+        
+
+    def test_reload_class_nonexist(self):
+        
+
+    def test_reload_not_json(self):
+        
 
 if __name__ == "__main__":
     unittest.main()
