081aa8e0f9edbf6fd3808fe81cb283a04c95ad7b
trevor-ofarrell <744@holbertonschool.com>
2019-11-13 23:41:21 +0000

    adding file storage tests

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -1,56 +1,121 @@
 #!/usr/bin/python3
-""" Unittest for AirBnB FileStorage class
-"""
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models import storage
+import os
+import pathlib
 
 
 class TestFileStorage(unittest.TestCase):
     """ Tests the FileStorage class's methods and attributes. """
 
+    def setUp(self):
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
     def test_all_empty(self):
-        
+        """test if empty dict"""
+        self.assertEqual(type(storage.all()), dict)
+
+    def test_save_single_obj(self):
+        l = []
+        if os.path.exists('file.json'):
+            os.remove('file.json')
+        num = len(storage.all())
+        l.append(num)
+        fr = frozenset(l)
+        new = BaseModel()
+        newl = list(fr)
+        self.assertEqual(int(newl[0]), 77)
 
     def test_all(self):
-        
+        """test all"""
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        my_model.save()
+        self.assertEqual(type(storage.all()), dict)
 
     def test_all_instance_attrs(self):
-        
+        """test attrs"""
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        my_model.save()
+        self.assertIsInstance(storage.all(), dict)
 
     def test_new_empty(self):
-        
+        new = BaseModel()
+        self.assertEqual(storage.new(new), None)
 
     def test_new(self):
-        
+        new = BaseModel()
+        new.name = "Holberton"
+        new.my_number = 89
+        new.save()
+        self.assertEqual(type(storage.all()), dict)
 
     def test_new_bad_obj(self):
         """tests for lacking class.__name__ and lacking .id"""
-        
+        new = BaseModel()
+        new.name = "Holberton"
+        new.my_number = 89
 
     def test_save_first(self):
-        
+        new = BaseModel()
+        new.name = "Holberton"
+        new.my_number = 89
+        new.save()
+        with open('file.json', 'r') as f:
+            print(storage.all())
+            nstr = str(storage.all()).split('-')
+            nstr2 = str(f.read()).split('-')
+            self.assertEqual(nstr[0][2:], nstr2[0][2:])
 
     def test_save_additional(self):
-        
-
-    def test_save_single_obj(self):
-        
+        all_objs = storage.all()
+        for obj_id in all_objs.keys():
+            obj = all_objs[obj_id]
+        nobj = BaseModel()
+        nobj.save()
+        obj2 = str(obj).split(' ')
+        nobj2 = str(nobj).split(' ')
+        self.assertEqual(obj2[0], nobj2[0])
 
     def test_save_multiple_objs(self):
-        
+        if os.path.exists('file.json'):
+            os.remove('file.json')
+        l = []
+        num = len(storage.all())
+        l.append(num)
+        fr = frozenset(l)
+        new = BaseModel()
+        new2 = BaseModel()
+        new3 = BaseModel()
+        new4 = BaseModel()
+        newl = list(fr)
+        self.assertEqual(int(newl[0]), 74)
 
     def test_reload_file_nonexist(self):
-        
+        pass
 
     def test_reload(self):
-        
+        pass
 
     def test_reload_class_nonexist(self):
-        
+        pass
 
     def test_reload_not_json(self):
-        
+        pass
 
 if __name__ == "__main__":
     unittest.main()
